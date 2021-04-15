f3ae85359e17d8fd6ffbc7c857868389e160c851
Rob Johnson on Linux <rob.johnson@aggiemail.usu.edu>
2021-04-14 22:08:31 -0600

    Added a normalizer function to count how many of each type of flare are represented in a dataset.

---
+++ normalizer.py
@@ -0,0 +1,20 @@
+
+
+def counter(filename, lines=-1):
+	file = open(filename, mode='r')
+	# This is an iterator to exit early
+	row = 0
+	# This dictionary corresponds to the five levels of flare events, from highest to lowest, with Q meaning quiet, or no flare.
+	dict = {' "X"':0, ' "M"':1, ' "C"':2, ' "B"':3, ' "Q"':4}
+
+
+	hist = [0,0,0,0,0]
+	for line in file:
+		cat = line.split(":")[2].split(',')[0]
+		if row == lines:
+			return hist
+		else:
+			hist[dict[cat]] += 1
+
+	file.close()
+	return hist

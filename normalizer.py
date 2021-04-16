7c44af8e4630b735bf88f38bd11ec896a7b2c1ad
Rob Johnson on Linux <rob.johnson@aggiemail.usu.edu>
2021-04-16 11:55:37 -0600

    Added a subsample functionality. I am now about to start testing it.

--- normalizer.py
+++ normalizer.py
@@ -1,3 +1,6 @@
+import random
+import torch
+import json
 magDict = {
     'TOTUSJH': 0,
     'TOTBSQ': 1,
@@ -33,24 +36,27 @@ magDict = {
     'PIL_LEN': 31,
     'XR_MAX': 32
 }
+flares = {'X':0, 'M':1, 'C':2, 'B':3, 'Q':4}
+
 
 def counter(filename, lines=-1):
 	file = open(filename, mode='r')
 	# This is an iterator to exit early
 	row = 0
 	# This dictionary corresponds to the five levels of flare events, from highest to lowest, with Q meaning quiet, or no flare.
-	dict = {' "X"':0, ' "M"':1, ' "C"':2, ' "B"':3, ' "Q"':4}
 
 
 	hist = [0,0,0,0,0]
 	for line in file:
 		if 'nan' in line or 'NaN' in line:
 			continue
-		cat = line.split(":")[2].split(',')[0]
+		# This line splits the line into keys and such, then splits that for values, then gets the correct character out
+		cat = line.split(":")[2].split(',')[0][2]
 		if row == lines:
 			return hist
 		else:
-			hist[dict[cat]] += 1
+			hist[flares[cat]] += 1
+			row += 1
 
 	file.close()
 	return hist
@@ -65,11 +71,12 @@ def getDataFromJSON(path="data/train_partition1_data.json", device='cpu', earlyS
 	This function returns a tensor containing the data, a list containing the corresponding labels,
 	and a list that contains the counts of each type of solar flare.
 	'''
-		# This dataset is heavily skewed, so we need to get the number of each type of flare.
+	# This dataset is heavily skewed, so we need to get the number of each type of flare.
 	# This also lets us get the number of lines in the file with a sum.
 	# This function also ignores any lines with a value of NaN.
+	# This function also only gets the amount of lines we either want or need - it will stop at the earlier of earlystop and the end of file.
 	weights = counter(path, earlyStop)
-	lines = np.sum(weights)
+	length = np.sum(weights)
 	# Check when we want to stop - the end of the file or earlier.
 	if earlyStop < 0: length = lines
 	else: length = min(earlyStop, lines)
@@ -81,7 +88,6 @@ def getDataFromJSON(path="data/train_partition1_data.json", device='cpu', earlyS
 	# Dimensions: 0: number of entries we want. 1: the 33 fields in the data. 2: the 60 observations in each field. 
 	tnsr = torch.Tensor().new_empty((length, 33, 60), device=device)
 	labels = []
-	flares = {'X':0, 'M':1, 'C':2, 'B':3, 'Q':4}
 		
 	row = -1
 	for line in file:
@@ -114,3 +120,56 @@ def getDataFromJSON(path="data/train_partition1_data.json", device='cpu', earlyS
 	# Famous last words.
 	return tnsr, labels, weights
 
+
+def subSample(path="data/train_partition1_data.json", earlyStop=-1, device='cuda'):
+	# find where to stop - the minimum of our data, or the early stop
+	weights = counter(path)
+	stop = min(weights)
+	if earlyStop > 0: stop = min(stop, earlyStop)
+
+	file = open(path, mode='r') # standard read only file object
+	pos = 0 # this will update with the position of a line that we need read
+	indicies = [[],[],[],[],[]] # this list will store the locations of lines with an i level flare
+
+	for line in file:
+		if 'nan' in line or 'NaN' in line:
+			# Lines with NAN  would take more effort to use than it's worth. 
+			pos += len(line)
+			continue
+		# get the category, use the number assigned to it to place the line beginning in the correct list in indicies
+		cat = line.split(":")[2].split(',')[0][2] 
+		indicies[flares[cat]].append(pos)
+		pos += len(line)
+
+	# Choose which lines to read with a random sample, then shuffle them to get a real dataset back
+	finalIndicies = []
+	for i in range(5):
+		finalIndicies += random.sample(indicies[i], stop)
+	random.shuffle(finalIndicies)
+
+	tnsr = torch.Tensor().new_empty((stop*5, 33, 60), device=device) #Declare a tensor that can hold all our data
+	pos = 0 # reuse this variable, since we don't need it anymore for what it was doing
+	# read in the lines dictated by finalIndicies
+	for location in finalIndicies:
+		file.seek(location)
+		line=file.readline()
+
+		# Load the line as a dictionary. Row is an integer place and v is a smaller dictionary.
+		d: dict = json.loads(line)
+		pos += 1
+		if pos % 100 == 0:
+			print(f'Now loading event {pos}/{stop*5}')
+		for _, v in d.items(): # we use the _ because we don't want the ID.
+			# append the label to our list
+			labels.append(flares[v['label']])
+			
+			# Break each individual dictionary into dictionaries of observations
+			# Key is the string in magDict, and timeDict is a dictionary of observations over time
+			for key, timeDict in v['values'].items():
+				# Turn our name string into a numeric value
+				location = magDict[key]
+				# Get the measurements out of the time series dictionary
+				for timeStamp, measurement in timeDict.items():
+					tnsr[row][location][int(timeStamp)] = measurement
+	print(f'{pos} lines loaded.')
+	return tnsr
